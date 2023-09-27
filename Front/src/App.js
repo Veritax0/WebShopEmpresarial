489a9a420dc6835a15ff4493cd30c0c76d377317
cripto26 <daniel4826073@gmail.com>
2023-09-26 21:54:31 -0500

    Se comienza el desarrollo de los Template.

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="banner"> 
+        <button className="carrito"></button> 
+        <div className="banner_item_location">
+          <input type="text" placeholder="Buscar" />
+          <button className="boton_busqueda">Buscar</button>
+        </div>
+        <nav>
+              <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/nosotros">Nosotros</a></li>
+                <li><a href="/servicios">Servicios</a></li>
+              </ul>
+        </nav>
+      </div>
     </div>
   );
 }
