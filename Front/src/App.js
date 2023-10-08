37ecc975cb8bcec742ae5e855ad68725bfb6f146
Veritax0 <juan.verao@udea.edu.co>
2023-10-07 19:46:53 -0500

    Base de la vista de filtrado creado

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,35 +1,38 @@
 import './App.css';
 import CarouselComponent from './components/Carrousel';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div className="banner"> 
-        <div className='banner2'>
-          <div  className='columna'></div>
-          <div className='columna2'><div className="banner_item_location">
-            <input type="text" placeholder="Buscar" />
-            <button className="boton_busqueda">Buscar</button>
-          </div></div>
-          <div  className='columna'><button className="carrito"></button> </div>
+    <Router>
+      <div className="App">
+        <div className="banner"> 
+          <div className='banner2'>
+            <div  className='columna'></div>
+            <div className='columna2'><div className="banner_item_location">
+              <input type="text" placeholder="Buscar" />
+              <button className="boton_busqueda">Buscar</button>
+            </div></div>
+            <div  className='columna'><button className="carrito"></button> </div>
+          </div>
+          <Link to=".\Filtrado\FiltroProductos.js">Filtrar Productos</Link>
+          <nav>
+                  <ul>
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/nosotros">Nosotros</a></li>
+                    <li><a href="/servicios">Servicios</a></li>
+                  </ul>
+            </nav>
+          
         </div>
-        <nav>
-                <ul>
-                  <li><a href="/">Inicio</a></li>
-                  <li><a href="/nosotros">Nosotros</a></li>
-                  <li><a href="/servicios">Servicios</a></li>
-                </ul>
-          </nav>
-        
-      </div>
-      <div className="banner3">
-        <CarouselComponent></CarouselComponent>
-        <div>
-          <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAPCOj9Mg9sDlaVQki34-XMKq9KFs8PJSOeA&usqp=CAU'></img>
+        <div className="banner3">
+          <CarouselComponent></CarouselComponent>
+          <div>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAPCOj9Mg9sDlaVQki34-XMKq9KFs8PJSOeA&usqp=CAU'></img>
+          </div>
         </div>
       </div>
-    </div>
-    
+    </Router>
   );
 }
 
