ccfaee34e7d4842eae08ccf4480e1acdea41b735
cripto26 <daniel4826073@gmail.com>
2023-10-09 03:32:09 -0500

    Componente PromoBox creado

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,13 +1,15 @@
 import './styles/App.css';
 import Banner from './components/Banner';
 import SearchBox from './components/SearchBox';
+import PromoSetion from './components/PromoSection';
 
 const App =()=>{
 
   return(
       <div className="App">
         <div className="ItenLocation">
-           <Banner></Banner> <SearchBox></SearchBox>
+           <Banner></Banner> <SearchBox></SearchBox><PromoSetion></PromoSetion>
+        
         </div>
     </div>
   )
