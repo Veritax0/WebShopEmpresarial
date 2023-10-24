5fae74d9c9106edb5fba4b9bef6327506751956c
cripto26 <daniel4826073@gmail.com>
2023-10-24 00:00:10 -0500

    Vista al 90%

---
+++ Front/src/service/ProductService.jsx
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export class ProductService {
+    baseUrl = "http://localhost:3000/producto/";
+
+    getAll() {
+        return axios.get(this.baseUrl + "buscar").then(res => res.data);
+    }
+}
