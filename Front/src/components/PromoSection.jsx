de0de1e828b950e165e50930ecb3c9d18046b593
Veritax0 <juan.verao@udea.edu.co>
2023-10-27 17:23:54 -0500

    Conexión funcional

--- Front/src/components/PromoSection.jsx
+++ Front/src/components/PromoSection.jsx
@@ -4,40 +4,31 @@ import "../styles/PromoSection.css"
 import { ProductService } from '../service/ProductService';
 
 class PromoSection extends Component{
-
     constructor(){
         super();
-        this.state = {};
+        this.state = {
+            products: []
+        };
         this.ProductService = new ProductService();
-        this.products = []
-        this.ProductService.getAll().then(data => this.products.push(data))
-      }
+    }
     
-      componentDidMount(){
-        this.ProductService.getAll().then(data => this.setState({productos: data}))
-      }
-
-    getProducts(){
-        var productsArray = this.products
-        var productos = productsArray
-        return productos
+    componentDidMount(){
+        this.ProductService.getAll().then(data => this.setState({products: data}))
     }
 
     render(){
-        var products = this.getProducts()
-        var p = products[0]
-        console.log(products)
-        console.log(p)
+        var products = this.state.products;
         return (
             <div className="PromoSection">
                 <h1 className="promoTitle">Productos en Promoción y Nuevos</h1>
-                    <div className="promoProducts">
-                    {products.map(products => (
-                        <PromoBox key={products.id} product={products} />
+                <div className="promoProducts">
+                    {products.map(product => (
+                        <PromoBox key={product.id} product={product} />
                     ))}
                 </div>
             </div>
         );
     }
-};
+}
+
 export default PromoSection;
