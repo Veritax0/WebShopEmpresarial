953fac756554b6ebf5c4f274abe2440419fc865e
cripto26 <daniel4826073@gmail.com>
2023-10-30 14:35:42 -0500

    Vista detalle del producto

--- Front/src/components/PromoSection.jsx
+++ Front/src/components/PromoSection.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PromoBox from "./PromoBox";
 import "../styles/PromoSection.css"
-import { ProductService } from './service/ProductService';
+import { ProductService } from '../service/ProductService';
 
 class PromoSection extends Component{
 
--- Front/src/service/ProductService.jsx
+++ Front/src/service/ProductService.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-
 export class ProductService {
-    baseUrl = "http://localhost:3000/producto/";
+    baseUrl = "http://localhost:3000/";
 
     getAll() {
-        return axios.get(this.baseUrl + "buscar").then(res => res.data);
+        return axios.get(this.baseUrl ).then(res => res.data);
+    }
+
+    getById(id) {
+        return axios.get(this.baseUrl + id).then(res => res.data);
     }
 }
-
