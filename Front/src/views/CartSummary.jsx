ee5da0e35de3956b9ff733f483d9c9c3eae89bad
cripto26 <daniel4826073@gmail.com>
2024-05-20 14:39:35 -0500

    cart ready

--- Front/src/views/CartSummary.jsx
+++ Front/src/views/CartSummary.jsx
@@ -2,32 +2,87 @@ import React, { useContext } from 'react';
 import { CartContext } from '../contextos/CartContext.jsx';
 import "../styles/CartSummary.css";
 
-
-
 const CartSummary = () => {
-    const { cartItems } = useContext(CartContext);
+    const { cartItems, incrementQuantity, decrementQuantity, removeItem } = useContext(CartContext);
 
-
-    const totalPrice = cartItems.reduce((total, item) => total + item.precio * item.cantidad, 0);
-
+    const totalPrice = cartItems.reduce((total, product) => total + product.precio * product.cantidad, 0);
 
     return (
-        <div className="cart-summary">
-            {cartItems.map(item => (
-                <div key={item.id} className="cart-item">
-                    <h3>{item.nombre}</h3>
-                    <p>Cantidad: {item.cantidad}</p>
-                    <p>Precio: ${item.precio}</p>
+        <div className="h-screen bg-gray-100 pt-20">
+            <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
+            <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0">
+                <div className="rounded-lg md:w-2/3">
+                    {cartItems.map(product => (
+                        <div key={product.id} className="justify-between mb-6 rounded-lg bg-white p-6 shadow-md sm:flex sm:justify-start">
+                            <img src={`/${product.id}.png`} alt="product-image" className="w-full rounded-lg sm:w-40" />
+                            <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                                <div className="mt-5 sm:mt-0">
+                                    <h2 className="text-lg font-bold text-gray-900">{product.nombre}</h2>
+                                    <p className="mt-1 text-xs text-gray-700">{product.talla}</p>
+                                </div>
+                                <div className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
+                                    <div className="flex items-center border-gray-100">
+                                        <span 
+                                            className="cursor-pointer rounded-l bg-gray-100 py-1 px-3.5 duration-100 hover:bg-blue-500 hover:text-blue-50" 
+                                            onClick={() => decrementQuantity(product.id)}
+                                        > 
+                                            - 
+                                        </span>
+                                        <input 
+                                            className="h-8 w-8 border bg-white text-center text-xs outline-none" 
+                                            type="number" 
+                                            value={product.cantidad} 
+                                            min="1" 
+                                            readOnly
+                                        />
+                                        <span 
+                                            className="cursor-pointer rounded-r bg-gray-100 py-1 px-3 duration-100 hover:bg-blue-500 hover:text-blue-50" 
+                                            onClick={() => incrementQuantity(product.id)}
+                                        > 
+                                            + 
+                                        </span>
+                                    </div>
+                                    <div className="flex items-center space-x-4">
+                                        <p className="text-sm">{product.precio.toFixed(2)} $</p>
+                                        <svg 
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            fill="none" 
+                                            viewBox="0 0 24 24" 
+                                            strokeWidth="1.5" 
+                                            stroke="currentColor" 
+                                            className="h-5 w-5 cursor-pointer duration-150 hover:text-red-500"
+                                            onClick={() => removeItem(product.id)}
+                                        >
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
-            <h2 className="total-price">Total: ${totalPrice}</h2>
-            {/* Ejemplo de botón, si lo necesitas */}
-            {/* <a href="/checkout" className="button">Proceder al Pago</a> */}
+                <div className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+                    <div className="mb-2 flex justify-between">
+                        <p className="text-gray-700">Subtotal</p>
+                        <p className="text-gray-700">${totalPrice.toFixed(2)}</p>
+                    </div>
+                    <div className="flex justify-between">
+                        <p className="text-gray-700">envio</p>
+                        <p className="text-gray-700">$10000</p>
+                    </div>
+                    <hr className="my-4" />
+                    <div className="flex justify-between">
+                        <p className="text-lg font-bold">Total</p>
+                        <div>
+                            <p className="mb-1 text-lg font-bold">${(totalPrice).toFixed(2)} PES</p>
+                            <p className="text-sm text-gray-700">IVA incluido</p>
+                        </div>
+                    </div>
+                    <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Verificar</button>
+                </div>
+            </div>
         </div>
     );
-            }
-
-
-
+};
 
 export default CartSummary;
